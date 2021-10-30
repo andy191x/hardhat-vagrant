31366040c7b1fbf31e5d6395712ef93482a2ae3e
Andrew Davis <andy@191x.com>
2021-10-30 18:53:49 -0400

    Install solc

--- provision.sh
+++ provision.sh
@@ -18,6 +18,17 @@ apt install -y nodejs
 which node || exit 1
 which npm || exit 1
 
+# Install solc
+# From: https://github.com/ethereum/solidity/releases
+pushd /usr/local/bin
+wget -O solc-0.8.9 https://github.com/ethereum/solidity/releases/download/v0.8.9/solc-static-linux
+chmod +x solc-0.8.9
+if [ -f solc ]; then
+	unlink solc
+fi
+ln -s solc-0.8.9 solc
+popd
+
 # Add jump to project folder upon login (for dev.sh)
 echo 'cd ~/project' >> /root/.profile
 echo '' >> /root/.profile
